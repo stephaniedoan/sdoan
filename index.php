2fcca08e6ed7ba52042d28cf803af503e08ef72a
stephaniedoan <sdoan@csumb.edu>
2017-08-29 20:22:55 +0000

    adding index again

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Stephanie CST 352</title>
+        <title>Stephanie Doan's CST 352</title>
     </head>
     <body>
-
+<p>Stephanie Doan's CST 352</p>
     </body>
 </html>
